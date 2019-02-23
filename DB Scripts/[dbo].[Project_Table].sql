23b03b40a15318002b9cde876099ae2ec0e1282d
gopalit <45861265+gopalit@users.noreply.github.com>
2019-02-23 00:25:25 -0700

    Update

--- DB Scripts/[dbo].[Project_Table].sql
+++ DB Scripts/[dbo].[Project_Table].sql
@@ -1,11 +1,18 @@
-USE [ProjectManager]
+USE [FSDDB]
 GO
 
-/****** Object:  Table [dbo].[Project_Table]    Script Date: 30-09-2018 01:04:41 ******/
+/****** Object:  Table [dbo].[Project_Table]    Script Date: 2/23/2019 12:24:59 AM ******/
+DROP TABLE [dbo].[Project_Table]
+GO
+
+/****** Object:  Table [dbo].[Project_Table]    Script Date: 2/23/2019 12:24:59 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
 GO
 
 CREATE TABLE [dbo].[Project_Table](
@@ -19,6 +26,10 @@ CREATE TABLE [dbo].[Project_Table](
 	[Project_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
 GO
 
 
