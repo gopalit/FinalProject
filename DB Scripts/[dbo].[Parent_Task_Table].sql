bafc72b01ce4e1f41a35041bd1ae87e62ca60f6e
gopalit <45861265+gopalit@users.noreply.github.com>
2019-02-23 00:26:17 -0700

    Update

--- DB Scripts/[dbo].[Parent_Task_Table].sql
+++ DB Scripts/[dbo].[Parent_Task_Table].sql
@@ -1,11 +1,18 @@
-USE [ProjectManager]
+USE [FSDDB]
 GO
 
-/****** Object:  Table [dbo].[Parent_Task_Table]    Script Date: 27-09-2018 15:20:31 ******/
+/****** Object:  Table [dbo].[Parent_Task_Table]    Script Date: 2/23/2019 12:25:43 AM ******/
+DROP TABLE [dbo].[Parent_Task_Table]
+GO
+
+/****** Object:  Table [dbo].[Parent_Task_Table]    Script Date: 2/23/2019 12:25:43 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
 GO
 
 CREATE TABLE [dbo].[Parent_Task_Table](
@@ -16,6 +23,10 @@ CREATE TABLE [dbo].[Parent_Task_Table](
 	[Parent_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
 GO
 
 
